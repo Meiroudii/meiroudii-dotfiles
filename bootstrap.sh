40561d27027efbc1b25340cd49c3c004b448063d
Meiroudii <119019700+Meiroudii@users.noreply.github.com>
2023-08-16 15:49:05 +0800

    Initial backups

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,3 +3,7 @@
 echo "Soon this will be finnish"
 echo "Arch-base only current available"
 sudo pacman -S zsh git htop neovim feh i3-wm rofi
+
+sleep 2
+echo "All packages has been installed"
+clear
